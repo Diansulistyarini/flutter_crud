e432f769d065f421fe11606b646ba2cdc67148dd
Diansulistyarini <diansulistyarini07@gmail.com>
2021-02-07 23:54:19 +0700

    first commit

---
+++ lib/crud.dart
@@ -0,0 +1,35 @@
+import 'package:sqflite/sqflite.dart';
+import 'class_penangkap.dart';
+import 'access_database.dart';
+
+class CRUD {
+AccessDatabase dbHelper = new AccessDatabase();
+Future<int> insert(ClassPenangkap todo) async {
+    Database db = await dbHelper.initDb();
+    int count = await db.insert('contact', todo.toMap());
+    return count;
+  }
+Future<int> update(ClassPenangkap todo) async {
+    Database db = await dbHelper.initDb();
+    int count = await db.update('contact', todo.toMap(),
+        where: 'id=?', whereArgs: [todo.id]);
+    return count;
+  }
+Future<int> delete(ClassPenangkap todo) async {
+    Database db = await dbHelper.initDb();
+    int count =
+        await db.delete('contact', where: 'id=?', whereArgs: [todo.id]);
+    return count;
+  }
+Future<List<ClassPenangkap>> getContactList() async {
+    Database db = await dbHelper.initDb();
+    List<Map<String, dynamic>> mapList =
+        await db.query('contact', orderBy: 'name');
+    int count = mapList.length;
+    List<ClassPenangkap> contactList = List<ClassPenangkap>();
+    for (int i = 0; i < count; i++) {
+      contactList.add(ClassPenangkap.fromMap(mapList[i]));
+    }
+    return contactList;
+  }
+}
